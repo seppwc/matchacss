f5d1c78a09c393c950756bb35080b07ea9a0b239
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2020-08-11 15:32:07 +0100

    initial commit

---
+++ scripts/buildthemes.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const { spawn } = require('child_process');
+
+fs.readdir(path.join(__dirname, '..', 'src/themes'), (err, files) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  files.forEach((f) => {
+    const srcPath = path.resolve(
+      __dirname,
+      path.join(__dirname, '..', 'src/themes', f)
+    );
+
+    const distPath = path.resolve(
+      __dirname,
+      path.join(__dirname, '..', 'dist/themes', f.split('.')[0] + '.css')
+    );
+
+    const child = spawn('sass', [srcPath, distPath]);
+  });
+});
